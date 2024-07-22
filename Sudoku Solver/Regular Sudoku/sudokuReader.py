2084f5bad215c6f097a576ee577759453e7efd09
leeyanleryan <leeyanleryan.21@gmail.com>
2024-07-22 21:44:31 +0800

    binary number image

--- Sudoku Solver/Regular Sudoku/sudokuReader.py
+++ Sudoku Solver/Regular Sudoku/sudokuReader.py
@@ -74,25 +74,62 @@ class sudokuReader:
                 return top_left, (top_left[0]+499, top_left[1]+499)
         return (-1, -1), (-1, -1)
     
+    def removeExtraColor(self, number_image):
+        is_empty = True
+        for k in range(number_image.shape[0]):
+            for l in range(number_image.shape[1]):
+                rgb = self.getRGBAt(number_image, k, l)
+                if rgb == [255,255,255]:
+                    continue
+                if rgb == self.border_rgb:
+                    is_empty = False
+                    continue
+                self.changeRGBAt(number_image, k, l, [255,255,255])
+        return number_image, is_empty
+    
+    def resizeNumberImage(self, start, change, i, j):
+        height, width = change[i], change[j]
+        
+        if height == 54 and width == 54:
+            top_left = (start[i]+2, start[j]+2)
+            bottom_right = (top_left[0]+49, top_left[1]+49)
+        elif height == 54 and width == 53:
+            top_left = (start[i]+2, start[j]+1)
+            bottom_right = (top_left[0]+49, top_left[1]+49)
+        elif height == 54 and width == 52:
+            top_left = (start[i]+2, start[j]+1)
+            bottom_right = (top_left[0]+49, top_left[1]+49)
+
+        elif height == 53 and width == 54:
+            top_left = (start[i]+1, start[j]+2)
+            bottom_right = (top_left[0]+49, top_left[1]+49)
+        elif height == 53 and width == 53:
+            top_left = (start[i]+1, start[j]+1)
+            bottom_right = (top_left[0]+49, top_left[1]+49)
+        elif height == 53 and width == 52:
+            top_left = (start[i]+1, start[j]+1)
+            bottom_right = (top_left[0]+49, top_left[1]+49)
+
+        elif height == 52 and width == 54:
+            top_left = (start[i]+1, start[j]+2)
+            bottom_right = (top_left[0]+49, top_left[1]+49)
+        elif height == 52 and width == 53:
+            top_left = (start[i]+1, start[j]+1)
+            bottom_right = (top_left[0]+49, top_left[1]+49)
+        elif height == 52 and width == 52:
+            top_left = (start[i]+1, start[j]+1)
+            bottom_right = (top_left[0]+49, top_left[1]+49)
+
+        return self.image[top_left[0]:bottom_right[0]+1, top_left[1]:bottom_right[1]+1]
+
     def saveNumbers(self):
         start = [2, 58, 113, 168, 223, 278, 334, 388, 443]
         change = [54, 53, 52, 53, 53, 53, 52, 53, 54]
-        curr = 115
+        curr = 77
         for i in range(9):
             for j in range(9):
-                top_left = (start[i], start[j])
-                bottom_right = (top_left[0]+change[i], top_left[1]+change[j])
-                number_image = self.image[top_left[0]:bottom_right[0]+1, top_left[1]:bottom_right[1]+1]
-                is_empty = True
-                for k in range(number_image.shape[0]):
-                    for l in range(number_image.shape[1]):
-                        rgb = self.getRGBAt(number_image, k, l)
-                        if rgb == [255,255,255]:
-                            continue
-                        if rgb == self.border_rgb:
-                            is_empty = False
-                            continue
-                        self.changeRGBAt(number_image, k, l, [255,255,255])
+                number_image = self.resizeNumberImage(start, change, i, j)
+                number_image, is_empty = self.removeExtraColor(number_image)
                 if not is_empty:
                     self.saveImage(f"Numbers/{str(curr)}.png", number_image)
                     curr += 1
@@ -103,28 +140,32 @@ class sudokuReader:
         for i in range(9):
             row = []
             for j in range(9):
-                top_left = (start[i], start[j])
-                bottom_right = (top_left[0]+change[i], top_left[1]+change[j])
-                number_image = self.image[top_left[0]:bottom_right[0]+1, top_left[1]:bottom_right[1]+1]
-                is_empty = True
-                for k in range(number_image.shape[0]):
-                    for l in range(number_image.shape[1]):
-                        rgb = self.getRGBAt(number_image, k, l)
-                        if rgb == [255,255,255] or rgb == self.border_rgb:
-                            continue
-                        if rgb == self.border_rgb:
-                            is_empty = False
-                            continue
-                        self.changeRGBAt(number_image, k, l, [255,255,255])
+                number_image = self.resizeNumberImage(start, change, i, j)
+                number_image, is_empty = self.removeExtraColor(number_image)
                 if is_empty:
                     number = 0
                 else:
                     number = self.readNumber(number_image)
                 row.append(number)
             self.sudoku.append(row)
+
+    def convertNumberToBinary(self, number_image):
+        output = []
+        for i in range(number_image.shape[0]):
+            row = []
+            for j in range(number_image.shape[1]):
+                if self.getRGBAt(number_image, i, j) == [255, 255, 255]:
+                    row.append("0")
+                else:
+                    row.append("1")
+            output.append(row)
+        return output
     
     def readNumber(self, number_image):
+        number_image_binary = self.convertNumberToBinary(number_image)
+        for row in number_image_binary:
+            print("".join(row))
+        print()
         return 0
         
 sr = sudokuReader("Puzzles/puzzle7.png")
-print(sr.sudoku)
